9ab004e70208ffe7c65c3986cd04fd9400c1d311
Ekaterina <ekaterina@Ekaterinas-MacBook-Pro.local>
2018-03-21 20:48:19 +0000

    my_first_app

--- my_first_app/hello.py
+++ my_first_app/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("MyApp")
 
@@ -13,5 +13,16 @@ def exist():
 @app.route("/wednesday")
 def wednesday():
     return "It's Wednesday!"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    print(form_data["email"])
+    return "All OK"
 
 app.run(debug=True)
